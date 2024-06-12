80a9a14d5575130bc20795a10033f46df4a9c0b8
Ahmad Jbara <ahmadjbara@Gmail.com>
2024-06-12 09:50:06 +0300

    AfifF

---
+++ GlobusSatProject/src/SubSystemModules/Communication/TRXVU.c
@@ -0,0 +1,49 @@
+/*
+ * TRXVU.c
+/*
+ * trxuv_operations.c
+ *
+ *  Created on: Jul 4, 2012
+ *      Author: marcoalfer
+ */
+
+
+#include <satellite-subsystems/IsisAntS.h>
+
+#include <TRXVU.h>
+
+int InitTrxvu(){
+	ISIStrxvuI2CAddress myTRXVUAddress;
+	ISIStrxvuFrameLengths myTRXVUFramesLength ;
+
+	//Buffer def
+	myTRXVUFramesLength.maxAX25frameLengthTX=SIZE_TXFRAME;
+	myTRXVUFramesLength.maxAX25frameLengthRX=SIZE_RXFRAME;
+
+	//I2C add def
+	myTRXVUAddress.addressVu_rc=I2C_TRXVU_RC_ADDR;
+	myTRXVUAddress.addressVu_tc=I2C_TRXVU_TC_ADDR;
+
+	//Bitrate def
+	ISIStrxvuBitrate myTRXVUBitrates;
+	myTRXVUBitrates=trxvu_bitrate_9600;
+	if(logError(IsisTrxvu_initialize(&myTRXVUAddress,&myTRXVUFramesLength,&myTRXVUBitrates,1),"Initialize-TRXVU"))
+		return -1;
+	vTaskDelay(1000); //wait a little
+
+    ISISantsI2Caddress myAntennaAddress;
+    myAntennaAddress.addressSideA=ANTS_I2C_SIDE_A_ADDR;
+    myAntennaAddress.addressSideB=ANTS_I2C_SIDE_B_ADDR;
+
+    //init the ants sysytem
+    if(logError(IsisTrxvu_initialize(&myAntennaAddress,1),"InitTrxvu-IsisAntS_initialize"))
+    	return -1;
+    InitTxModule();
+    InitBeaconParams();
+    InitSemaphores();
+    //checkTransponderStart();//let us see if we need to turn on the tx
+
+return 0;
+}
+
+
